7430b2081a27c00931e6154d00c63f671cb3d15c
Ichfan Arif Widyanto <64718802+DUTRA-Mandiri@users.noreply.github.com>
2021-12-22 06:04:16 +0700

    Create ATmega128-megaCoreBoard.ino

---
+++ examples/ATmega128-megaCoreBoard/ATmega128-megaCoreBoard.ino
@@ -0,0 +1,26 @@
+#include  "dimmerMegaCore.h"
+
+#define InterruptPin 6 // use pin D6 (INT 6)
+#define TriacPin 13
+
+int potValue = 0;
+int duty;
+
+void setup() {
+  Serial.begin(115200);
+  // begin(Interupt Pin, Triac Pin, Frequency (50/60 Hz), Output Active High or Low)
+  dimmerMegaCore.begin(InterruptPin, TriacPin, 50, LOW);
+}
+
+void loop() {
+
+  // read the analog in value:
+  potValue = analogRead(A0);
+  // map it to the range of the analog out: 1-100%
+  duty = map(potValue, 0, 1023, 1, 100);
+  
+  Serial.print("duty : ");
+  Serial.println(duty);
+  dimmerMegaCore.setPower(duty); // 1-100%
+  delay(100); 
+}
